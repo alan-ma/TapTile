40bcad15228f128fa9f36c761ac52b002790f812
Alan Ma <alan@katsudon.local>
2017-03-09 17:47:02 -0500

    tile adding functionality added

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var tiles = [
-	{
-		one: 0,
-		two: 1,
-		three: 0,
-		four: 0
+var rows = 0;
+function randint(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+function addRow(correct) {
+	var new_id = 'row' + rows.toString();
+	$('#game').prepend('<div id="' + new_id + '" class="row"></div>');
+	for (i=0; i<4; i++) {
+		if (i == correct) {
+			$('#' + new_id).append('<div class="tile correct" data-correct="1"></div>');
+		} else {
+			$('#' + new_id).append('<div class="tile" data-correct="0"></div>');
+		}
 	}
-];
-var score = 0;
-function addRow() {
-
+	$('#' + new_id + ' .tile').click(function() {
+		addRow(randint(0,4));
+	});
+	rows += 1;
 }
-function update() {
-
-}
-
-(function() {
-
-})();
+$(document).ready(function() {
+	var score = 0;
+	(function doTimeout (i) {
+		setTimeout(function () {
+			if (i--) {
+				addRow(randint(0,4));
+				doTimeout(i);
+			}
+		}, 0);
+	})(4);
+});
