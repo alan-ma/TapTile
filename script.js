5b688c0f4e35df7a001768fef750c0596b1dad89
Alan Ma <alan@katsudon.local>
2017-03-09 20:36:17 -0500

    added score mechanic

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 var rows = 0;
+var current_row = 0;
+var score = 0;
 function randint(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
+}
+function youLose() {
+	alert("You lose.\nFinal score: " + score.toString() + ".\nGood job!");
+	score = 0;
+	$('#score').html(score);
 }
 function addRow(correct) {
 	var new_id = 'row' + rows.toString();
 	$('#game').prepend('<div id="' + new_id + '" class="row"></div>');
 	for (i=0; i<4; i++) {
 		if (i == correct) {
-			$('#' + new_id).append('<div class="tile correct" data-correct="1"></div>');
+			$('#' + new_id).append('<div class="tile correct" data-correct="1" data-row="' + rows.toString() + '"></div>');
 		} else {
 			$('#' + new_id).append('<div class="tile" data-correct="0"></div>');
 		}
 	}
 	$('#' + new_id + ' .tile').click(function() {
-		addRow(randint(0,4));
+		if ($(this).attr('data-correct') == '1' && $(this).attr('data-row') == current_row.toString()) {
+			$(this).removeClass('correct');
+			$(this).addClass('clicked');
+			addRow(randint(0,4));
+			current_row += 1;
+			score += 1;
+			$('#score').html(score);
+		} else {
+			youLose();
+		}
 	});
 	rows += 1;
 }
 $(document).ready(function() {
-	var score = 0;
 	(function doTimeout (i) {
 		setTimeout(function () {
 			if (i--) {
@@ -28,5 +43,5 @@ $(document).ready(function() {
 				doTimeout(i);
 			}
 		}, 0);
-	})(4);
+	})(3);
 });
